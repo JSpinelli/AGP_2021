980c595d915caabea7887170a8115b36a661e27e
Julian <julianspi95@gmail.com>
2021-02-27 12:53:19 -0300

    Implemented empty space detection for capture

--- Problem Sets/Assets/Week 4/GoSolver.cs
+++ Problem Sets/Assets/Week 4/GoSolver.cs
@@ -2,26 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-struct GoData
-{
-    public char typeOfObject;
-    public List<Vector2> blobItBelongs;
-}
+
+
 
 public class GoSolver : MonoBehaviour
 {
+    public struct Coord
+    {
+        public int x;
+        public int y;
+    }
+    
+   public  struct GoData
+    {
+        public char typeOfObject;
+        public List<Coord> blobItBelongs;
+    }
+
     public TextAsset[] myGoBoards;
 
     // Start is called before the first frame update
     public GameObject boardSlot;
     public GameObject blackChip;
     public GameObject whiteChip;
+
+    public GameObject nextWhiteChip;
+    public GameObject nextBlackChip;
+    
     private GoData[,] board;
     private int width;
     private int height;
 
-    private List<List<Vector2>> blackBlobs = new List<List<Vector2>>();
-    private List<List<Vector2>> whiteBlobs = new List<List<Vector2>>();
+    private List<List<Coord>> blackBlobs = new List<List<Coord>>();
+    private List<List<Coord>> whiteBlobs = new List<List<Coord>>();
 
     void Start()
     {
@@ -72,7 +85,7 @@ public class GoSolver : MonoBehaviour
             {
                 if (i != height - 1 && j != width - 1)
                     Instantiate(boardSlot, new Vector3(j + 0.5f, -i - 0.5f, 0f), Quaternion.identity);
-                List<Vector2> myBlob = CheckMyself(j, i);
+                List<Coord> myBlob = CheckMyself(j, i);
                 if (myBlob != null)
                 {
                     CheckNeighbors(j, i, myBlob);
@@ -82,16 +95,76 @@ public class GoSolver : MonoBehaviour
 
         Debug.Log("Number of White Blobs: " + whiteBlobs.Count);
         Debug.Log("Number of Black Blobs: " + blackBlobs.Count);
+
+        foreach (var whiteBlob in whiteBlobs)
+        {
+            CheckBlob(whiteBlob);
+        }        
+        
+        foreach (var blackBlob in blackBlobs)
+        {
+            CheckBlob(blackBlob);
+        }
     }
 
-    public List<Vector2> CheckMyself(int j, int i)
+    public void CheckBlob(List<Coord> currentBlob)
+    {
+        List<Coord> emptySpaces = new List<Coord>();
+        foreach (var piece in currentBlob)
+        {
+            if (piece.x - 1 >= 0)
+            {
+                CheckEmpty(piece.x-1,piece.y, emptySpaces);
+            }
+            if (piece.x + 1 < width)
+            {
+                CheckEmpty(piece.x+1,piece.y, emptySpaces);
+            }
+            if (piece.y - 1 >= 0)
+            {
+                CheckEmpty(piece.x,piece.y-1, emptySpaces);
+            }
+            if (piece.y + 1 < height)
+            {
+                CheckEmpty(piece.x,piece.y+1, emptySpaces);
+            }
+        }
+
+        if (emptySpaces.Count == 1)
+        {
+            Coord firstPoint = currentBlob[0];
+            Instantiate(
+                board[firstPoint.x, firstPoint.y].typeOfObject == 'W' ? nextBlackChip : nextWhiteChip,
+                new Vector3(emptySpaces[0].x, -emptySpaces[0].y, -1f),
+                Quaternion.identity);
+        }
+    }
+
+    public void CheckEmpty(int x, int y, List<Coord> emptySpaces)
+    {                    
+        if (board[x, y].typeOfObject == ' ')
+        {
+            Coord point = new Coord();
+            point.x = x;
+            point.y = y;
+            if (!emptySpaces.Exists(match => match.x == point.x && match.y == point.y))
+            {
+                emptySpaces.Add(point);
+            }
+        }
+    }
+
+    public List<Coord> CheckMyself(int j, int i)
     {
         if (board[j, i].typeOfObject != ' ')
         {
             if (board[j, i].blobItBelongs == null)
             {
-                var newList = new List<Vector2>();
-                newList.Add(new Vector2(j, i));
+                var newList = new List<Coord>();
+                Coord point = new Coord();
+                point.x = j;
+                point.y = i;
+                newList.Add(point);
                 if (board[j, i].typeOfObject == 'W')
                 {
                     
@@ -104,20 +177,23 @@ public class GoSolver : MonoBehaviour
                 blackBlobs.Add(newList);
                 return newList;
             }
-
+            Instantiate( board[j, i].typeOfObject == 'W'? whiteChip: blackChip, new Vector3(j, -i, -1f), Quaternion.identity);
             return board[j, i].blobItBelongs;
         }
 
         return null;
     }
 
-    public void CheckNeighbors(int j, int i, List<Vector2> myBlob)
+    public void CheckNeighbors(int j, int i, List<Coord> myBlob)
     {
         if (j + 1 < width)
         {
             if (board[j, i].typeOfObject == board[j + 1, i].typeOfObject)
             {
-                myBlob.Add(new Vector2(j + 1, i));
+                Coord point = new Coord();
+                point.x = j+1;
+                point.y = i;
+                myBlob.Add(point);
                 board[j + 1, i].blobItBelongs = myBlob;
             }
         }
@@ -126,7 +202,10 @@ public class GoSolver : MonoBehaviour
         {
             if (board[j, i].typeOfObject == board[j, i + 1].typeOfObject)
             {
-                myBlob.Add(new Vector2(j, i + 1));
+                Coord point = new Coord();
+                point.x = j;
+                point.y = i+1;
+                myBlob.Add(point);
                 board[j, i + 1].blobItBelongs = myBlob;
             }
         }
