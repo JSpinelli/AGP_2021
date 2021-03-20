8ccd285b9567496c3718342400738e2c4c7977b3
Julian <julianspi95@gmail.com>
2021-03-20 13:19:07 -0300

    Tried to make a AssetBundle manager, added a logger

---
+++ Problem Sets/Assets/Prototyping Kit/Logger.cs
@@ -0,0 +1,26 @@
+﻿//THIS SHOULD BE ADDED TO THE TOP OF THE CLASS USING THE LOGGER
+//#if UNITY_EDITOR
+//#define ENABLE_LOGS
+//#endif
+using System.Diagnostics;
+
+public static class Logger
+{
+    [Conditional("ENABLE_LOGS")]
+    public static void Log(string logMsg)
+    {
+        UnityEngine.Debug.Log(logMsg);
+    }
+    
+    [Conditional("ENABLE_LOGS")]
+    public static void Warning(string logMsg)
+    {
+        UnityEngine.Debug.LogWarning(logMsg);
+    }
+    
+    [Conditional("ENABLE_LOGS")]
+    public static void Error(string logMsg)
+    {
+        UnityEngine.Debug.LogError(logMsg);
+    }
+}
---
+++ Problem Sets/Assets/TestScript.cs
@@ -0,0 +1,14 @@
+﻿#if UNITY_EDITOR
+#define ENABLE_LOGS
+#endif
+using UnityEngine;
+
+
+public class TestScript : MonoBehaviour
+{
+    void Start()
+    {
+        Logger.Log("THIS IS A TEST");
+    }
+    
+}
